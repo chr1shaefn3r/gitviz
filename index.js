c0fc6c27bb83d639a63222d96335c8cad90e992e
Christoph Haefner <christophhaefner@web.de>
2015-08-19 20:00:55 +0200

    Added all missing styles + comprehensive example

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var util = require('util'),
 var style = {
 	blob: ["box", "filled", "#ddddff", "#bbbbff"],
 	tree: ["oval", "filled", "#99ff99"],
+	commit: ["hexagon", "filled", "#ffff99"],
+	reference: ["box", "filled", "#9999ff"],
+	tag: ["box", "filled", "#67fdf3"],
+	head: ["box", "filled", "#ff9999"],
 	apply: function(node, style) {
 		node.set("shape", style[0]);
 		node.set("style", style[1]);
@@ -14,17 +18,53 @@ var style = {
 
 // Create digraph G
  var g = graphviz.digraph("G");
-// Add node (ID: Hello)
-var n1 = g.addNode( "323f" );
-style.apply(n1, style.blob);
 
-// Add node (ID: World)
-var n2 = g.addNode( "d52a" );
-style.apply(n2, style.tree);
+// Add blobs
+var b1 = g.addNode( "323f" );
+style.apply(b1, style.blob);
+var b2 = g.addNode( "70c3" );
+style.apply(b2, style.blob);
 
-// Add edge between the two nodes
-var e = g.addEdge( n2, n1 );
-e.set( "label", "stuff.txt" );
+// Add trees
+var t1 = g.addNode( "d52a" );
+style.apply(t1, style.tree);
+var t2 = g.addNode( "edb0" );
+style.apply(t2, style.tree);
+
+// Add commits
+var c1 = g.addNode( "4b3e" );
+style.apply(c1, style.commit);
+var c2 = g.addNode( "bdcf" );
+style.apply(c2, style.commit);
+
+// Add tags
+var ta1 = g.addNode( "67a8" );
+style.apply(ta1, style.tag);
+
+// Add refs
+var r1 = g.addNode( "refs/heads/master" );
+style.apply(r1, style.reference);
+var r2 = g.addNode( "refs/tags/rc1" );
+style.apply(r2, style.reference);
+
+// Add HEAD
+var head = g.addNode( "HEAD" );
+style.apply(head, style.head);
+
+// Add edges between nodes
+var e1 = g.addEdge( t1, b1 );
+e1.set( "label", "stuff.txt" );
+var e2 = g.addEdge( t2, b2 );
+e2.set( "label", "foobar.txt" );
+var e3 = g.addEdge( t2, b1 );
+e3.set( "label", "stuff" );
+g.addEdge( c1, t1 );
+g.addEdge( c2, c1 );
+g.addEdge( c2, t2 );
+g.addEdge( r1, c1 );
+g.addEdge( r2, ta1 );
+g.addEdge( ta1, c2 );
+g.addEdge( head, r1 );
 
 // Print the dot script
 console.log( g.to_dot() ); 
