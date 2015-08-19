4569faa12cb555105b6cbeaf98464adef710c589
Christoph Haefner <christophhaefner@web.de>
2015-08-19 19:37:56 +0200

    Added blob and tree styles

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 var util = require('util'),
-	  graphviz = require('graphviz');
+	graphviz = require('graphviz');
+
+var style = {
+	blob: ["box", "filled", "#ddddff", "#bbbbff"],
+	tree: ["oval", "filled", "#99ff99"],
+	apply: function(node, style) {
+		node.set("shape", style[0]);
+		node.set("style", style[1]);
+		node.set("fillcolor", style[2]);
+		node.set("color", style[3]);
+	}
+};
 
 // Create digraph G
  var g = graphviz.digraph("G");
 // Add node (ID: Hello)
-var n1 = g.addNode( "Hello", {"color" : "blue"} );
-n1.set( "style", "filled" );
+var n1 = g.addNode( "323f" );
+style.apply(n1, style.blob);
 
 // Add node (ID: World)
-g.addNode( "World" );
+var n2 = g.addNode( "d52a" );
+style.apply(n2, style.tree);
 
 // Add edge between the two nodes
-var e = g.addEdge( n1, "World" );
-e.set( "color", "red" );
+var e = g.addEdge( n2, n1 );
+e.set( "label", "stuff.txt" );
 
 // Print the dot script
 console.log( g.to_dot() ); 
 
-// Set GraphViz path (if not in your path)
-g.setGraphVizPath( "/usr/bin" );
 // Generate a PNG output
 g.output( "png", "test01.png" );
+
