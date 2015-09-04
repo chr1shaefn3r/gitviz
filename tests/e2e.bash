80466323bb0f134e75c051fec0edea014bb5f718
Christoph Haefner <christophhaefner@web.de>
2015-09-04 18:45:44 +0200

    Added four more e2e tests

--- tests/e2e.bash
+++ tests/e2e.bash
@@ -6,15 +6,20 @@ function m_success() { echo -e " \033[1;32m✔\033[0m $@"; }
 function m_error() { echo -e " \033[1;31m✖\033[0m $@"; }
 function m_arrow() { echo -e " \033[1;33m➜\033[0m $@"; }
 
+function commit() {
+	GIT_AUTHOR_DATE="Fri Sep 13 10:10:10 2015 +0200" \
+		GIT_COMMITTER_DATE="Fri Sep 13 10:10:10 2015 +0200" \
+		git commit  --author "Christoph Haefner <chr1shaefn3r@christophhaefner.de>" -m $1 1> /dev/null
+}
 function assert() {
-	cmp "git-internals.pdf" "../../comparedata/"$1".pdf";
+	cmp "git-internals.pdf" "../../comparedata/"$1".pdf" &> /dev/null
 	if [ $? -eq 1 ]; then
 		m_error "Test "$1" failed :("
-		exit 1;
 	else
 		m_success "Test "$1" passed :)"
 	fi
 }
+
 
 GITVIZ=`pwd`"/cli.js"
 
@@ -27,7 +32,7 @@ cd $TEMP_DIR
 m_header "Working in: "`pwd`
 
 TEMP_GIT="test-git-repo"
-rm -r $TEMP_GIT || true
+rm -rf $TEMP_GIT || true
 
 git init $TEMP_GIT
 cd $TEMP_GIT
@@ -41,3 +46,24 @@ assert empty_repo
 echo "Hello, World!" | git hash-object -w --stdin 1> /dev/null
 $GITVIZ
 assert one_blob
+
+echo "Hello, World!!" | git hash-object -w --stdin 1> /dev/null
+$GITVIZ
+assert two_blob
+
+echo "Hello, World!" > hello.txt
+git add hello.txt
+commit "Initial"
+$GITVIZ
+assert one_commit
+
+echo "Hello, World!" >> hello.txt
+git add hello.txt
+commit "Second"
+$GITVIZ
+assert two_commit
+
+git tag v1
+$GITVIZ
+assert tag
+
